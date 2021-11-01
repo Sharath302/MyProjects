305d453daadff041be8e4e65e67b119bd59a73c3
Sharath302 <sharath.ka302@gmail.com>
2021-11-01 21:41:20 +0530

    NewCommit

---
+++ if.py
@@ -0,0 +1,8 @@
+# Find a number is odd or even
+
+num = int(input("Enter a number:"))
+
+if num%2 == 0:
+    print("The entered number is even and the number is:",num)
+else:
+    print("The entered number is odd and the number is:",num)
