2293ff591c4885ca50fcbf7346140858d98434bb
Sharath K Achar <sharath.ka302@gmail.com>
2021-12-13 20:23:00 +0530

    Delete if.py

--- if.py
+++
@@ -1,8 +0,0 @@
-# Find a number is odd or even
-
-num = int(input("Enter a number:")) # Accpecting the number from the user
-
-if num%2 == 0: # Checking the number is odd or even
-    print("The entered number is even and the number is:",num) 
-else:
-    print("The entered number is odd and the number is:",num)
